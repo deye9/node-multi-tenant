49718726d30713b050968174de1e23e9a4181555
Adegbenga Adeye <adegbenga.adeye@outlook.com>
2019-02-10 03:50:39 +0100

    master

--- postinstall.sh
+++ postinstall.sh
@@ -4,26 +4,26 @@ CURRENT_DIR="`cd $1; pwd`";
 destinationPath="${INIT_CWD// /\\ }";
 finalPath="${destinationPath/$CURRENT_DIR/}";
 
-echo 'Changing over to the database directory.';
-cd './database';
+echo 'Changing over to the Distribution directory.';
+cd './dist';
 
 # echo "Attempting to create a tenants folder in the Migrations folder.";
-# mkdir -p ~/$finalPath/database/migrations/tenants;
+# mkdir -p ~/$finalPath/dist/migrations/tenants;
 
 # echo "Moving all existing migration files to the tenants folder.";
-# mv ~/$finalPath/database/migrations/*.js ~/$finalPath/database/migrations/tenants
+# mv ~/$finalPath/dist/migrations/*.js ~/$finalPath/dist/migrations/tenants
 
 # copy each file/dir to user dir(~/)
  for node in `ls`
   do
 
-    echo 'Copying' './database/'$node 'over to ~'$finalPath'/database/'$node;
-    cp -R $node/*.js ~/$finalPath/database/$node;
+    echo 'Copying' './dist/'$node 'over to ~'$finalPath'/dist/'$node;
+    cp -R $node/*.js ~/$finalPath/dist/$node;
 
  done
 
 
 cd '..'
-echo 'Exited the database directory';
+echo 'Exited the Distribution directory';
 
 # chmod +x postinstall.sh
