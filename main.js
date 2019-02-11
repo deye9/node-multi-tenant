6b2e2f7b314295805f4f47b5054093dbce73fc30
Adegbenga Adeye <adegbenga.adeye@outlook.com>
2019-02-11 16:56:17 +0100

    master

--- main.js
+++ main.js
@@ -17,13 +17,6 @@ let app = {};
  */
 app.init = () => {
   // Start the CLI
-  console.log('1 = ', __dirname);
-  console.log('2 = ', path.dirname(__dirname));
-  console.log('3 = ', process.env.TENANT_PATH);
-  // /Users/andelatsm/Desktop/Projects/NodeApps/ESB/node_modules/node_multi_tenant
-  
-  //   let tenantPath = path(__dirname);
-
   cliInit();
 };
 
