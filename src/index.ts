b99e89b2b46170783c3b1e3fc53ff78d38c542b8
Adeye Adegbenga <adegbenga.adeye@outlook.com>
2023-12-25 11:12:01 +0000

    feat: docker setup (#29)
    docker setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 const greeting = 'world';
 
 export function hello(world: string = greeting): string {
+  // eslint-disable-next-line no-console
+  console.log(`Hello ${world}!`);
   return `Hello ${world}!`;
 }
+
+hello(); // Hello world!
